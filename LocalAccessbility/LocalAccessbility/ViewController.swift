1ba6fd299b6909abfe2c21d2b6206122b19bd8c9
apwierk2451 <apwierk2451@gmail.com>
2022-08-18 14:10:11 +0900

    feat: 화폐단위, 날짜, 이미지, 언어 등 지역, 언어에 따른 변경 테스트

--- LocalAccessbility/LocalAccessbility/ViewController.swift
+++ LocalAccessbility/LocalAccessbility/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         bottomLabel.frame = CGRect(x: 20, y: view.bounds.size.height - 150, width: view.bounds.size.width - 40, height: 50)
         
         bottomLabel.text = "Touch the button below!"
+        bottomLabel.text = String(format: NSLocalizedString("Touch the button below!", comment: "인사"))
         
         bottomLabel.font = UIFont.systemFont(ofSize: 20)
         
@@ -34,10 +35,12 @@ class ViewController: UIViewController {
         button.frame = CGRect(x: 20, y: view.bounds.size.height - 80, width: view.bounds.size.width - 40, height: 50)
         
         button.setTitle("Touch Me!", for: .normal)
+        button.setTitle(String(format: NSLocalizedString("Touch Me!", comment: "ds")), for: .normal)
         
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        
+
         button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
         
         view.addSubview(button)
     }
@@ -47,7 +50,30 @@ class ViewController: UIViewController {
         addBottomLabel()
         addButton()
         
+        
         dateTimeLabel.text = Date().description
     }
+    
+    @objc func tapButton() {
+        currency()
+        date()
+    }
+    
+    func currency() {
+        let locale = Locale.current
+        let price = 3000.33 as NSNumber
+        let formatter = NumberFormatter()
+        
+        formatter.numberStyle = .currency
+        formatter.currencyCode = locale.currencyCode
+        formatter.locale = locale
+        
+        currencyLabel.text = formatter.string(from: price)
+    }
+    
+    func date() {
+        let date = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .short)
+        
+        dateTimeLabel.text = date
+    }
 }
-
